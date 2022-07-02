c05c9f2178b396ee90e30ffd522abb993614e4f0
mauroslucios <mauroslucios@gmail.com>
2022-07-02 18:14:33 -0300

    Update departamento entity

--- src/main/java/br/com/cronos/employees/entity/Departamento.java
+++ src/main/java/br/com/cronos/employees/entity/Departamento.java
@@ -7,13 +7,9 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Table(name="tb_departamento")
-@Getter
-@Setter
 @AllArgsConstructor
 public class Departamento {
 
@@ -25,4 +21,22 @@ public class Departamento {
 	public Departamento() {
 		
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+
 }
