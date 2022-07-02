58147b1aec9fe365bfe461c8c7424013f8b3569b
mauroslucios <mauroslucios@gmail.com>
2022-07-02 13:33:45 -0300

    Funcionario controller criado

---
+++ src/main/java/br/com/cronos/employees/controller/FuncionarioController.java
@@ -0,0 +1,12 @@
+package br.com.cronos.employees.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/api/v1")
+@CrossOrigin(origins="*")
+public class FuncionarioController {
+
+}
