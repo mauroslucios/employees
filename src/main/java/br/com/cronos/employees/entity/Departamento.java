4341d084a812c30a9adc423f8d0c4d6f01758725
mauroslucios <mauroslucios@gmail.com>
2022-07-03 22:01:57 -0300

    Entidade departamento atualizada

--- src/main/java/br/com/cronos/employees/entity/Departamento.java
+++ src/main/java/br/com/cronos/employees/entity/Departamento.java
@@ -1,10 +1,12 @@
 package br.com.cronos.employees.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
 import lombok.AllArgsConstructor;
 
@@ -16,6 +18,9 @@ public class Departamento{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotBlank
+	@Column(unique=true)
 	private String nome;
 	
 	public Departamento() {
