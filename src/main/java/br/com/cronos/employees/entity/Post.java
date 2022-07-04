c2f6345a3381636e541d14f191eaa9255c8a400a
mauroslucios <mauroslucios@gmail.com>
2022-07-03 22:02:55 -0300

    Entidade post atualizada

--- src/main/java/br/com/cronos/employees/entity/Post.java
+++ src/main/java/br/com/cronos/employees/entity/Post.java
@@ -1,10 +1,14 @@
 package br.com.cronos.employees.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -20,6 +24,17 @@ public class Post {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotBlank
+	@Column(unique=true)
 	private String descricao;
+	
+	@NotBlank
+	@Column(unique=true)
 	private String titulo;
+	
+	@ManyToOne
+	@JoinColumn(name="funcionario_id")
+	private Funcionario funcionario;
+	
 }
