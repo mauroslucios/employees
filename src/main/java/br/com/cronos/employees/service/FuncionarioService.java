21778edc6489c8902f9156ff855015a16f79f2b3
mauroslucios <mauroslucios@gmail.com>
2022-07-06 13:24:03 -0300

    FuncionarioService atualizado

--- src/main/java/br/com/cronos/employees/service/FuncionarioService.java
+++ src/main/java/br/com/cronos/employees/service/FuncionarioService.java
@@ -24,19 +24,40 @@ public class FuncionarioService {
 		return funcionarioRepository.findAll();
 	}
 	
+	/**
+	 * 
+	 * @param id
+	 * @return faz a busca um funcionário pelo id no banco
+	 * @throws ObjectNotFoundException
+	 */
 	public Funcionario buscarUnicoFuncionario(Long id) throws ObjectNotFoundException{
 		Optional<Funcionario> funcionario = funcionarioRepository.findById(id);
 		return funcionario.orElseThrow(()-> new ObjectNotFoundException(null, "Funcionário não encontrado!"));
 	}
 	
+	/**
+	 * 
+	 * @param funcionario
+	 * @return faz a inserção de um funcionário no banco 
+	 */
 	public Funcionario inserirFuncionario(Funcionario funcionario) {
 		return funcionarioRepository.save(funcionario);
 	}
 	
+	/**
+	 * 
+	 * @param id deleta um funcionário pelo id no banco
+	 * 
+	 */
 	public void deletaUmFuncionario(Long id) {
 		funcionarioRepository.deleteById(id);
 	}
 	
+	/**
+	 * 
+	 * @param objFuncionario
+	 * @return atualiza um funcionário pelo id no banco
+	 */
 	public Funcionario atualizarUmFuncionario(Funcionario objFuncionario) {
 		Funcionario funcionario = buscarUnicoFuncionario(objFuncionario.getId());
 		funcionario.setNome(objFuncionario.getNome());
