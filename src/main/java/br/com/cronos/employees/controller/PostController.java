ae3a15837999b50c0982416372999b610ef45df0
mauroslucios <mauroslucios@gmail.com>
2022-07-06 13:23:33 -0300

    PostController atualizado

--- src/main/java/br/com/cronos/employees/controller/PostController.java
+++ src/main/java/br/com/cronos/employees/controller/PostController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -39,11 +40,11 @@ public class PostController {
 	 * 
 	 * @return retorna uma lista de posts
 	 */
-	@GetMapping(value="/post")
+	@GetMapping(value="/posts", produces="application/json")
 	@ApiOperation(value = "Retorna uma lista de posts")
 	@ApiResponses(value = {
 			@ApiResponse(code = 200, message = "Posts encontrados com sucesso!"),
-			@ApiResponse(code = 400, message = "Verifique sua requisição alo deu errado."),
+			@ApiResponse(code = 400, message = "Verifique sua requisição algo deu errado."),
 			@ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso."),
 			@ApiResponse(code = 404, message = "Recurso não encontrado ou movido."),
 			@ApiResponse(code = 500, message = "Aconteceu uma exceção.")
@@ -64,7 +65,7 @@ public class PostController {
 	 * @param id
 	 * @return retorna um único post pelo id
 	 */
-	@GetMapping(value="/posts/{id}")
+	@GetMapping(value="/posts/{id}",produces="application/json")
 	@ApiOperation(value = "Retorna um único post pelo id")
 	@ApiResponses(value = {
 		@ApiResponse(code = 200, message = "Post encontrado com sucesso!"),
@@ -73,7 +74,7 @@ public class PostController {
 		@ApiResponse(code = 404, message = "Recurso não encontrado ou movido."),
 		@ApiResponse(code = 500, message = "Aconteceu uma excessão.")
 	})
-	public ResponseEntity<Post> buscarUnicoPost(@PathVariable Long id){
+	public ResponseEntity<Post> buscarUnicoPost(@PathVariable(value="id") Long id){
 		Post post = postService.buscarUnicoPost(id);
 		post.add(linkTo(methodOn(PostController.class).buscarTodosPosts()).withRel("Listar todos os posts"));
 		return ResponseEntity.ok().body(post);
@@ -85,7 +86,7 @@ public class PostController {
 	 * @return faz a inserção de um post no banco
 	 */
 	@ResponseStatus(HttpStatus.CREATED)
-	@PostMapping(value="/posts")
+	@PostMapping(value="/posts",produces="application/json")
 	@ApiResponses(value= {
 		@ApiResponse(code = 201, message = "Post cadstrado com sucesso!"),
 		@ApiResponse(code = 400, message = "Verifique sua requisição algo deu errado."),
@@ -104,7 +105,7 @@ public class PostController {
 	 * @param id
 	 * @return deleta um post do banco pelo id
 	 */
-	@DeleteMapping(value="/posts/{id}")
+	@DeleteMapping(value="/posts/{id}",produces="application/json")
 	@ApiOperation(value="Deleta um post do banco pelo id")
 	@ApiResponses(value= {
 			@ApiResponse(code = 204, message = "Post deletado com sucesso!"),
@@ -122,14 +123,18 @@ public class PostController {
 	 * @param id
 	 * @return atualiza um post no banco pelo id
 	 */
+	@PutMapping(value="/posts/{id}", produces="application/json")
+	@ApiOperation(value="Atualiza um post pelo id")
+	@ApiResponses(value= {
+			@ApiResponse(code = 204, message = "Post alterado com sucesso!"),
+			@ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+			@ApiResponse(code = 500, message = "Aconteceu gerada uma exceção")
+	})
 	public ResponseEntity<Void> atualizaUmPost(@RequestBody Post objPost, @PathVariable Long id){
 		objPost.setId(id);
 		postService.atualizaUmPost(objPost);
 		return ResponseEntity.noContent().build();
 	}
 	
-	
-	
-	
-	
+
 }
